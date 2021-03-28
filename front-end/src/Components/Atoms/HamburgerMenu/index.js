1662de096ff94b151671c5f5d5ff62d64f96a24a
alcantara2509 <alcantara2509@gmail.com>
2021-03-28 17:51:01 -0300

    add hamburger menu icon

---
+++ front-end/src/Components/Atoms/HamburgerMenu/index.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import HamburgerMenu from 'react-hamburger-menu';
+
+const HamburgerMenuComp = () => {
+  const [menuClicked, SetMenuClicked] = useState(false)
+  const toggleMenu = () => SetMenuClicked(menuClicked ? false : true);
+  return (
+    <HamburgerMenu
+      isOpen={menuClicked}
+      menuClicked={toggleMenu}
+      width={32}
+      height={18}
+      strokeWidth={2}
+      // rotate={0}
+      // color='black'
+      // borderRadius={0}
+      // animationDuration={0.5}
+    />
+  );
+};
+
+export default HamburgerMenuComp;

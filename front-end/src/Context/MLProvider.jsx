06b9fd702335b00a63f7ddeefdde2860cb686bc0
alcantara2509 <alcantara2509@gmail.com>
2021-04-04 23:26:57 -0300

    refactoring allVillas to context

--- front-end/src/Context/MLProvider.jsx
+++ front-end/src/Context/MLProvider.jsx
@@ -1,14 +1,28 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable import/no-extraneous-dependencies */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MLContext from './MLContext';
+import { fecthVillas } from '../Services/fetchVillas';
 
 function MLProvider({ children }) {
   const [isFetching, setIsFetching] = useState(false);
+  const [allVillas, setAllVillas] = useState([]);
+
+  useEffect(() => {
+    setIsFetching(true);
+
+    (async () => {
+      const apiResponse = await fecthVillas();
+      setAllVillas(apiResponse);
+      setIsFetching(false);
+    })();
+  }, []);
 
   const ContextValue = {
     isFetching,
     setIsFetching,
+    allVillas,
+    setAllVillas,
   };
 
   return (
--- front-end/src/Pages/Villas/index.jsx
+++ front-end/src/Pages/Villas/index.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 import React, { useEffect, useState, useContext } from 'react';
-import { fecthVillas } from '../../Services/fetchVillas';
 import MLContext from '../../Context/MLContext';
 import Loading from '../../Components/Atoms/Loading';
 import renderVillas from '../../Utils/renderVillas';
@@ -9,21 +8,10 @@ import { ButtonSeeAll } from '../../Components/Atoms';
 import './style.css';
 
 const Villas = () => {
-  const { isFetching, setIsFetching } = useContext(MLContext);
+  const { isFetching, allVillas } = useContext(MLContext);
 
-  const [allVillas, setAllVillas] = useState([]);
   const [seeAll, setSeeAll] = useState(10);
   const [reload, setReload] = useState('');
-
-  useEffect(() => {
-    setIsFetching(true);
-
-    (async () => {
-      const apiResponse = await fecthVillas();
-      setAllVillas(apiResponse);
-      setIsFetching(false);
-    })();
-  }, []);
 
   useEffect(() => {
     const favoriteVillas = JSON.parse(localStorage.getItem('favoriteVillas'));

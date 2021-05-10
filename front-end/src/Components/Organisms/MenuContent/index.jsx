535290ac104aeb984ff612873a0f31e1a9977082
alcantara2509 <alcantara2509@gmail.com>
2021-05-10 02:37:24 -0300

    menu translation

--- front-end/src/Components/Organisms/MenuContent/index.jsx
+++ front-end/src/Components/Organisms/MenuContent/index.jsx
@@ -1,21 +1,35 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import languages from '../../../Utils/languages';
+import { english, portuguese, spanish } from './languages';
 import './style.css';
 
 const MenuContent = ({ toggle, lang }) => (
   <section className={toggle}>
     <section className="links-container">
       <NavLink className="links" exact to="/" activeClassName="selected">Home</NavLink>
-      <NavLink className="links" to="/villas" activeClassName="selected">Villas</NavLink>
-      <NavLink className="links" to="/yachts" activeClassName="selected">Yachts</NavLink>
-      <NavLink className="links" to="/services" activeClassName="selected">24/7 Services</NavLink>
-      <NavLink className="links" to="/events" activeClassName="selected">Event Planning</NavLink>
-      <NavLink className="links" to="/measures" activeClassName="selected">Covid-19 Measures</NavLink>
+      <NavLink className="links" to="/villas" activeClassName="selected">
+        {languages(english, portuguese, spanish).link1}
+      </NavLink>
+      <NavLink className="links" to="/yachts" activeClassName="selected">
+        {languages(english, portuguese, spanish).link2}
+      </NavLink>
+      <NavLink className="links" to="/services" activeClassName="selected">
+        {languages(english, portuguese, spanish).link3}
+      </NavLink>
+      <NavLink className="links" to="/events" activeClassName="selected">
+        {languages(english, portuguese, spanish).link4}
+      </NavLink>
+      <NavLink className="links" to="/measures" activeClassName="selected">
+        {languages(english, portuguese, spanish).link5}
+      </NavLink>
       {/* <NavLink className="links" to="/blog" activeClassName="selected">Blog</NavLink> */}
     </section>
     <section className="wish-link-container">
-      <NavLink className="links" to="/wishlist" activeClassName="selected">My Wishlist</NavLink>
+      <NavLink className="links" to="/wishlist" activeClassName="selected">
+        {languages(english, portuguese, spanish).link6}
+      </NavLink>
     </section>
     <section className="languages-container">
       {lang}
---
+++ front-end/src/Components/Organisms/MenuContent/languages.js
@@ -0,0 +1,26 @@
+export const english = {
+  link1: 'Villas',
+  link2: 'Yachts',
+  link3: '24/7 Services',
+  link4: 'Event Planning',
+  link5: 'Covid-19 Measures',
+  link6: 'My Wishlist',
+};
+
+export const portuguese = {
+  link1: 'Vilas',
+  link2: 'Iates',
+  link3: 'Serviços 24/7',
+  link4: 'Eventos',
+  link5: 'Covid-19',
+  link6: 'Wishlist',
+};
+
+export const spanish = {
+  link1: 'Villas',
+  link2: 'Yachts',
+  link3: '24/7 Services',
+  link4: 'Event Planning',
+  link5: 'Covid-19 Measures',
+  link6: 'My Wishlist',
+};

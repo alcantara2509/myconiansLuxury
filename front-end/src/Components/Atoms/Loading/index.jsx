6af23b96501bd6d19afb0d7d7bc8961653b17dff
alcantara2509 <alcantara2509@gmail.com>
2021-05-12 12:55:50 -0300

    fix loading component

--- front-end/src/Components/Atoms/Loading/index.jsx
+++ front-end/src/Components/Atoms/Loading/index.jsx
@@ -1,9 +1,19 @@
+/* eslint-disable no-undef */
 /* eslint-disable react/prop-types */
 import React from 'react';
 import ReactLoading from 'react-loading';
+import { iconLogo } from '../../../assets';
+import './style.css';
 
-const Loading = ({ type, color }) => (
-  <ReactLoading type={type} color={color} height="26%" width="26%" />
-);
+const Loading = ({ type }) => {
+  const loadingSize = () => (window.innerWidth < 800 ? '24%' : '10%');
+
+  return (
+    <section className="loading-container">
+      <img src={iconLogo} alt="logo" className="logo-loading" />
+      <ReactLoading type={type} color="#1C3C45" height={loadingSize()} width={loadingSize()} />
+    </section>
+  );
+};
 
 export default Loading;

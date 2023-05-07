dcc16f5aa673c4aff8a9ac3de3aeb0c64d65a185
unknown <alcantara2509@gmail.com>
2023-05-07 16:37:22 -0300

    fix: change email outlook

--- src/Components/Organisms/Footer/index.jsx
+++ src/Components/Organisms/Footer/index.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { HashLink as Link } from 'react-router-hash-link';
+import React from "react";
+import { HashLink as Link } from "react-router-hash-link";
 import {
-  iconFacebook, iconInstagram, iconPinterest, iconArrowUp,
-} from '../../../assets';
-import './style.css';
+  iconFacebook,
+  iconInstagram,
+  iconPinterest,
+  iconArrowUp,
+} from "../../../assets";
+import "./style.css";
 
 const Footer = () => (
   <footer className="footer-container">
@@ -16,16 +19,20 @@ const Footer = () => (
     <section className="footer-elements-container">
       <section>
         <h3 className="footer-subtitle">Contact</h3>
-        <p className="footer-p">Phone: ‎+30 694 093 0706</p>
-        <p className="footer-p">WhatsApp: ‎+30 694 093 0706</p>
-        <p className="footer-p">E-mail: contact@myconianluxury.com</p>
+        <p className="footer-p">Phone: +30 698 404 3180</p>
+        <p className="footer-p">WhatsApp: +30 698 404 3180</p>
+        <p className="footer-p">E-mail: myconianluxury@gmail.com</p>
       </section>
       <section className="social-container">
         <h3 className="footer-subtitle">Social</h3>
         <a target="_blank" href="https://www.facebook.com/" rel="noreferrer">
           <img src={iconFacebook} alt="LinkedIn" className="social-icons" />
         </a>
-        <a target="_blank" href="https://www.instagram.com/myconianluxury" rel="noreferrer">
+        <a
+          target="_blank"
+          href="https://www.instagram.com/myconianluxury"
+          rel="noreferrer"
+        >
           <img src={iconInstagram} alt="Instagram" className="social-icons" />
         </a>
         <a target="_blank" href="https://pinterest.com/" rel="noreferrer">
